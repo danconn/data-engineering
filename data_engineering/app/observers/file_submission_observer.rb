edc4d9ad4bff3dfb5070f423c9485b020e5f8e4e
root <root@daniel-desktop.(none)>
2012-09-21 10:51:33 -0400

    Remove observer

--- data_engineering/app/observers/file_submission_observer.rb
+++
@@ -1,8 +0,0 @@
-class FileSubmissionObserver < ActiveRecord::Observer
-  def after_create(record)
-    # now use FileImporter class to do the dirty
-    # work after the file has been uploaded/saved
-    DataEngineering::FileImporter.new(record.data_file
-      .queued_for_write[:original].path, record).import!
-  end
-end
