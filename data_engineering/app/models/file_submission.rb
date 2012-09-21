53c65d39de9b4825665131b81ab8e17874619761
Daniel Connolly <dan.dconnoll@gmail.com>
2012-09-21 18:07:35 -0400

    Undo non-functional change

--- data_engineering/app/models/file_submission.rb
+++ data_engineering/app/models/file_submission.rb
@@ -7,7 +7,6 @@ class FileSubmission < ActiveRecord::Base
   after_create do |record|
     # now use FileImporter class to do the dirty
     # work after the file has been uploaded/saved
-    DataEngineering::FileImporter.new(record.data_file
-      .queued_for_write[:original].path, record).import!
+    DataEngineering::FileImporter.new(record.data_file.queued_for_write[:original].path, record).import!
   end
 end
