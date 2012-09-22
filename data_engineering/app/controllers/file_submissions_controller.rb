ed6c80e368f705f2179cbe410d4be1318ce23c2f
Daniel Connolly <dan.dconnoll@gmail.com>
2012-09-22 10:34:24 -0400

    Modified controller and views to use RESTful path helpers isntead of using the action names

--- data_engineering/app/controllers/file_submissions_controller.rb
+++ data_engineering/app/controllers/file_submissions_controller.rb
@@ -12,7 +12,7 @@ class FileSubmissionsController < ApplicationController
     if @file_submission.save
       flash[:notice] = "You just uploaded $#{@file_submission.gross_revenue} " + 
         "worth of transactions"
-      redirect_to :action => "index"
+      redirect_to file_submissions_path
     else
       # go back to new so we can see
       # error messages - YAY!
