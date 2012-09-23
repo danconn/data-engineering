1c9c6ced3ada770bcb6827363e4a4cfae093b1ef
Daniel Connolly <dan.dconnoll@gmail.com>
2012-09-23 16:48:03 -0400

    Use paperclip validation instead of rails native for validates attachment_presence of data_file

--- data_engineering/app/models/file_submission.rb
+++ data_engineering/app/models/file_submission.rb
@@ -2,11 +2,12 @@ class FileSubmission < ActiveRecord::Base
   attr_accessible :gross_revenue, :name, :data_file
   has_attached_file :data_file
   
-  validates :data_file, :attachment_presence => true
+  validates_attachment :data_file, :attachment_presence => true
   
   after_create do |record|
     # now use FileImporter class to do the dirty
     # work after the file has been uploaded/saved
     DataEngineering::FileImporter.new(record.data_file.queued_for_write[:original].path, record).import!
   end
+  
 end
