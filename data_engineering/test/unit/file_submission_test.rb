d6b4c7a9a7105aa555c17c189aad58b80e010005
Daniel Connolly <dan.dconnoll@gmail.com>
2012-09-24 00:27:30 -0400

    update unit test

--- data_engineering/test/unit/file_submission_test.rb
+++ data_engineering/test/unit/file_submission_test.rb
@@ -1,28 +1,30 @@
 require 'test_helper'
 
 class FileSubmissionTest < ActiveSupport::TestCase
+  include FileSubmissionTestHelper
+  
+  
   # make sure presence validation works
   test "should not save without a file" do
-    file_submission = FileSubmission.new
-    assert !file_submission.save, "Saved the file submission without a file"
+    assert !@file_submission.valid?,
+      "File submission should not be valid without data_file"
+    assert !@file_submission.save,
+      "Saved the file submission without a file"
+    assert_equal "can't be blank",
+      @file_submission.errors[:data_file].join('')
   end
   
   # make sure file_submission is actually saving
   # with file
   test "should save with a file" do
-    file_submission = FileSubmission.new
-    # get file in separate variable so we
-    # can compare values later
-    data_file = File.new('test/fixtures/files/example_input.tab')
-    file_submission.data_file = data_file
-    assert file_submission.save, "Did not save the file submission with a file"
+    # set data file
+    @file_submission.data_file = @data_file
     
-    # make sure gross_revenue calculated is correct
-    assert_equal 95.0, file_submission.gross_revenue
+    assert @file_submission.valid?,
+      "File submission should be valid with data_file"
     
-    
-    
-    # make sure file is closed
-    data_file.close
+    assert @file_submission.save, 
+      "Did not save the file submission with a file"    
   end
+  
 end
