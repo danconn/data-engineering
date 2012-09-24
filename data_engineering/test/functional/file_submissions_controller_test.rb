13ac8c326c0fab2a8f4d128ae8709590d7a60ce2
Daniel Connolly <dan.dconnoll@gmail.com>
2012-09-24 00:26:26 -0400

    Added more functional checks

--- data_engineering/test/functional/file_submissions_controller_test.rb
+++ data_engineering/test/functional/file_submissions_controller_test.rb
@@ -21,21 +21,32 @@ class FileSubmissionsControllerTest < ActionController::TestCase
       }
     end
     
+    # test assignment of file_submission
+    file_submission = assigns(:file_submission)
+    assert_not_nil file_submission, "File submission from assigns is nil"
+    
+    # test file_submission equals one from db
+    new_file_submission = FileSubmission.find(file_submission.id)
+    assert_equal file_submission, new_file_submission,
+      "File submissions do not match"
+    
+      # make sure the correct flash message was shown
+    assert_equal "You just uploaded $#{file_submission.gross_revenue} worth of transactions", 
+      flash[:notice], "Success message was incorrect"
+    
     # make sure it was redirect to index
     assert_redirected_to file_submissions_path,
       "Create response was not redirected to the index page"
       
-    # make sure the correct flash message was shown
-    assert_equal "You just uploaded $#{assigns(:file_submission).gross_revenue} worth of transactions", 
-      flash[:notice], "Success message was incorrect"
-      
     # make sure file was uploaded
-    full_path = assigns(:file_submission).data_file.path # get rid of some ugliness
+    full_path = file_submission.data_file.path # get rid of some ugliness
     assert File.exists?(full_path),
       "File at #{full_path} does not exist on the server"
       
-    # get rid of the file
-    remove_test_upload_file(assigns(:file_submission))
+    # get rid of the file (should be moved
+    # to some sort of teardown, but this
+    # is the onyl test that should need it)
+    remove_test_upload_file(file_submission)
   end
   
   # test failure scenario with no file
@@ -46,14 +57,20 @@ class FileSubmissionsControllerTest < ActionController::TestCase
       post :create, :file_submission => {}
     end
     
+    # test no id was assigned
+    assert_nil assigns(:file_submission).id,
+      "File submission should not have an id"
+    
     # make sure new view rendered
-    assert_response :success
-    assert_template 'file_submissions/new',
+    assert_response :success, "Did not respond with 200 status"
+    assert_template 'file_submissions/new', 
       "Create response did not render new view"
       
     # make sure flash message is nil
-    assert_equal nil, 
-      flash[:notice], "Success message was populated"
+    assert_equal nil, flash[:notice], "Success message was populated"
+    
+    # check error message
+    assert_select 'li', :content => "Data file can&#x27;t be blank"
   end  
   
 end
